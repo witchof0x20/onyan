b8e1391f2514a4621f6acb9ca19fc7606853a12b
witchof0x20 <witchof0x20@protonmail.com>
2018-02-08 01:44:52 -0600

    Added GPL

--- src/consensus.rs
+++ src/consensus.rs
@@ -1,3 +1,19 @@
+// Copyright 2018 witchof0x20
+/*  This file is part of onyan.
+
+    onyan is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    onyan is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with onyan.  If not, see <http://www.gnu.org/licenses/>.
+*/
 use std::io::BufRead;
 use std::str::from_str;
 use nom::alphanumeric;
@@ -687,9 +703,9 @@ struct ConsensusDocument {
 /// Included in consensuses only for method 19 and later.
 struct Package {
     package_name: String,
-    version:      String,
-    url:          String,
-    digests:      Vec<Digest>,
+    version: String,
+    url: String,
+    digests: Vec<Digest>,
 }
 
 struct Digest {
